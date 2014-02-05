36c54e20284c7391334f6d882e764d8169c82413
MarcoFalke <falke.marco@gmail.com>
2014-02-05 09:19:55 +0100

    Add dependencies

---
+++ validateInput.m
@@ -0,0 +1,39 @@
+function validateInput( M, y, U_min, U_max)
+%FUN2 Summary of this function goes here
+%   Detailed explanation goes here
+
+%VALIDATEINPUTINF validates the input of the sphere decoding algorithms
+%   Throws an error if the input can not be decoded using one of the
+%   sphere decoding algorithms.
+
+n=size(M,1);
+
+if (~isInteger(U_min))||(~isInteger(U_max))
+    error('decode:notInteger',...
+        'U_min and U_max have to be integer')
+end
+
+if (U_min>U_max)
+    error('decode:inverseConstellation',...
+        'U_min can not be greater than U_max')
+end
+
+if any(diag(M)<0)
+    error('decode:negtiveDiag',...
+        'All diagonal elements must be positive')
+end
+if ~isLowerTriangular(M)
+    error('decode:notTriangular',...
+        'The input matrix has to be lower triangular')
+end
+if ~(rank(M)==n)
+    error('decode:linearDependent',...
+        'The input matrix has to be linearly independent')
+end
+if ~(length(y)==n)
+    error('decode:dimensionMismatch',...
+        'The input matrix and vector have to be of the same dimension')
+end
+
+end
+
